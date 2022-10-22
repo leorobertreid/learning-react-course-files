74424f5a4c57ec134a00af0354bfc42ab8b0107e
Leo Reid <leo.reid.mail@gmail.com>
2022-10-22 12:09:42 +0200

    first commit

---
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message"
+  });
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "Hello World" });
+    setMessage("Hello World");
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
+};
+
+export default UseStateObject;
