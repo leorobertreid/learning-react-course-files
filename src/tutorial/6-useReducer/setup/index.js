bd35e15292b1af0786cbb15cda647400b16454d7
Leo Reid <leo.reid.mail@gmail.com>
2022-10-25 14:48:34 +0200

    learned about useReducer hook

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,32 +3,82 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state, 
+      people: newPeople, 
+      isModalOpen: true, 
+      modalContent: 'Item added'
+    };
+  }
+  if (action.type === 'NO_VALUE') {
+    return {
+      ...state, 
+      isModalOpen: 
+      true, modalContent: 'Please enter value'
+    };
+  }
+
+  if (action.type === 'CLOSE_MODAL') {
+    return {...state, isModalOpen: false};
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const newPeople = state.people.filter((person) => person.id !== action.payload);
+    return {...state, people: newPeople};
+  }
+
+  throw new Error('no matching action type');
+}
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+}
+
 const Index = () => {
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (name) {
-      showModal(true);
-      setPeople([...people, {id: new Date().getTime().toString(), name}]);
+      const newItem = {id: new Date().getTime().toString(), name}
+      dispatch({type: 'ADD_ITEM', payload: newItem});
       setName('');
     } else{
-      showModal(true);
+      dispatch({type: 'NO_VALUE'});
     }
-  }
+  };
+
+  const closeModal = () => {
+    dispatch({type: 'CLOSE_MODAL'});
+  };
 
   return (
     <>
-    {showModal && <Modal></Modal>}
-    <form onSubmit={handleSubmit}>
+    {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent}></Modal>}
+    <form onSubmit={handleSubmit} className='form'>
       <div>
         <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
       </div>
       <button type='submit'>add</button>
     </form>
+    {state.people.map((person) => {
+      return (
+      <div key={person.id} className='item'>
+        <h4>{person.name}</h4>
+        <button onClick={() => dispatch(
+          {type: 'REMOVE_ITEM', payload: person.id})}>
+            remove
+          </button>
+      </div>
+      );
+    })}
     </>
   );
 };
